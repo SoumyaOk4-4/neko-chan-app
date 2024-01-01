4547e6c850bb6664647f44653bd4b9119bbd4eaf
Soumya Chakraborty <94857677+SoumyaOk4-4@users.noreply.github.com>
2024-01-02 00:04:36 +0530

    w

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import React, { useState } from 'react';
 import Axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // UI & LOGOS
 import KawaiiNekoFactBtn from './assets/pictures/kawaii_nekofact_btn.png';
@@ -10,11 +12,23 @@ import NekoLogo from './assets/pictures/logo.png';
 
 function App() {
   const [nekoFact, setNekoFact] = useState("");
+  const [_20, set_20] = useState(0);
 
   // NEKO-FACT FUNC
   const fetchNekoFact = () => {
     Axios.get("https://catfact.ninja/fact").then((res) => {
       setNekoFact(res.data.fact);
+      congrats_20();
+    });
+  }
+
+  const congrats_20 = () => {
+    set_20(prevCount => {
+      const newCount = prevCount + 1;
+      if (newCount === 21) {
+        toast.info(`Congratulations! You've read ${_20} Neko(Cat) facts!`);
+      }
+      return newCount;
     });
   }
 
@@ -34,7 +48,7 @@ function App() {
       <footer>
         It's a prototype so wait a bit cat lovers. The app is under development.
         <br />
-        <b>&copy; 2023-2024 Neko Senpai</b>
+        <b>© 2023-2024 Neko Senpai</b>
         <br />
         <img className='neko-neko-btn' src={NekoLogo} alt='Neko Neko' />
       </footer>
