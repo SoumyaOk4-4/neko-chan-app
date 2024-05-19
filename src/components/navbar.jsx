a8b3cd499e2fd7a6d5e52adf9a2c6d60e4e150e8
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2024-05-20 00:15:40 +0530

    m

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import HomeIcon from '../assets/pictures/icons/HomeIcon.png';
 import ProfileIcon from '../assets/pictures/icons/ProfileIcon.png';
 
 const Navbar = () => {
     return (
         <ul className="navbar" type='none'>
-            <li> {/* may change in future */}
-                <a href="https://neko-chan-backend-production.up.railway.app/" className="nav-item home-up">
-                    <img src={HomeIcon} alt="Home" />
-                </a>
-            </li>
             <li> {/* may change in future */}
                 <a href="https://neko-chan-backend-production.up.railway.app/profile" className="nav-item">
                     <img src={ProfileIcon} alt="Profile" />
